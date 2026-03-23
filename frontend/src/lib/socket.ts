2a963176edd3cbfb7bd54831832067f47742d6eb
Poojith P <poojithpagadekal26@gmail.com>
2026-03-23 09:29:22 +0530

    fix: update socket url to socket url instead of api url

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const URL = import.meta.env.VITE_API_URL ?? "http://localhost:3000";
+const URL = import.meta.env.VITE_SOCKET_URL ?? "http://localhost:3000";
 
 export const socket: Socket = io(URL, {
   autoConnect: false,
